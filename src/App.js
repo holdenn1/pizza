18aee3fb8dcdb3305e8eb2c38a17bc85a36511d0
Ihor_Ivliev <igor852745@gmail.com>
2020-06-24 15:28:03 +0300

    join axios, fetch pizzas from json and using PropTypes

--- src/App.js
+++ src/App.js
@@ -1,16 +1,26 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import axios from 'axios'
 import {Route} from 'react-router-dom'
 import {Header} from "./components";
 import {Home, Cart} from "./Pages";
 
 
-
 function App() {
+
+    const [pizzas, setPizzas] = useState([])
+
+    useEffect(() => {
+        axios.get('http://localhost:3000/db.json').then(({data}) => {
+            setPizzas(data.pizzas)
+        })
+    }, [])
+
+
     return (
         <div className="wrapper">
             <Header/>
             <div className="content">
-                <Route path='/' exact component={Home}/>
+                <Route path='/' exact render={() => <Home items={pizzas}/>}/>
                 <Route path='/cart' exact component={Cart}/>
             </div>
         </div>
--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,13 +1,21 @@
 import React from "react";
+import PropTypes from 'prop-types'
 import classNames from 'classnames'
 
-function Button({className, outline, children}) {
+function Button({onClick, className, outline, children}) {
     return (
-        <button className={classNames('button', className, {'button--outline': outline,
-        })}>
+        <button
+            onClick={onClick}
+            className={classNames('button', className, {
+                'button--outline': outline,
+            })}>
             {children}
         </button>
     )
+}
+
+Button.propTypes = {
+    onClick: PropTypes.func,
 }
 
 export default Button
